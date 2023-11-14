ca46ecb845e5db6b02f2f3a7606b389551065835
Nikita Matskovsky <n.matskovsky@octogroup.io>
2023-11-14 20:27:17 +0400

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Header from './components/Header/Header'
 import HomePage from './pages/Home'
+import styles from "./main.module.css"
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main style={{ padding: '2rem 6rem' }}>
+      <main className={`${styles.main}`}>
         <HomePage />
       </main>
     </>
